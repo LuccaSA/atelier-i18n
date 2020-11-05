65cb516beb8e8226518f7f5602e10ffffbfd64bc
lucien bertin <me@lucienbertin.com>
2020-11-05 14:39:51 +0100

    first dra6ft of a cache - no expiration

--- src/app/principal/principal.cache.ts
+++ src/app/principal/principal.cache.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
 import { IPrincipal } from './principal.interface';
 
+const PRINCIPAL_KEY = 'lucca-principal-value';
 @Injectable()
 export class PrincipalCache {
+
 	get(): IPrincipal | null {
-		return null;
+		const cachedPrincipalRaw = localStorage.getItem(PRINCIPAL_KEY);
+		if (!cachedPrincipalRaw) {
+			return null;
+		}
+
+		const cachedPrincipal = JSON.parse(cachedPrincipalRaw) as IPrincipal;
+		return cachedPrincipal;
 	}
 	set(principal: IPrincipal): void {
-
+		localStorage.setItem(PRINCIPAL_KEY, JSON.stringify(principal));
 	}
 }
